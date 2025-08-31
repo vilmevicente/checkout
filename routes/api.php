c87367cc8ee820cac589fb15c4cf3d2ca9110f5c
No limits <vilme.vicente@gmail.com>
2025-09-01 00:34:43 +0100

    fix product redirect webhook

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-/* Route::prefix('pagfan')->group(function () {
+ Route::prefix('pagfan')->group(function () {
     Route::get('balance', [PagFanController::class, 'getBalance']);
     Route::post('qrcode', [PagFanController::class, 'generateQrCode']);
     Route::post('payment', [PagFanController::class, 'makePayment']);
     Route::post('webhook', [PagFanController::class, 'webhook']);
-}); */
+}); 
